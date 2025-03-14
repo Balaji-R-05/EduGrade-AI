0134eec1059f67d681c3f87d53ac4968716609cb
just-bm <tbalancemurugan@gmail.com>
2025-03-14 21:11:01 +0530

    teacher

--- smart-grading/src/App.jsx
+++ smart-grading/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
+import Teacher from '../src/pages/Teacher/Teacher';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/teacher" element={<Teacher/>}/>
       </Routes>
     </Router>
   );
